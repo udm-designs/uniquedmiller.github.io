60ca7f4bbe3a400d96b796a28036216a8f3cf163
Unique M <uniquedmiller@gmail.com>
2024-06-25 22:07:19 -0400

    Project Nav Completion

--- alt/styles/js/main.js
+++ alt/styles/js/main.js
@@ -1,4 +1,22 @@
-// Modals
+// 1. Project Navigation
+$(document).ready(function() {
+    // Event listener using event delegation for all li elements under #p-menu
+    $('#p-menu').on("click", ".libtn", function() {
+      // Remove 'active' class from all li elements
+      $('#p-menu .libtn').removeClass('active');
+  
+      // Add 'active' class to the clicked li element
+      $(this).addClass('active');
+  
+      // Toggle corresponding accordion section
+      var id = $(this).attr('id').slice(0, -3); // Extract id without 'btn'
+      $('.accord').removeClass('active');
+      $('#' + id + 'p').addClass('active');
+    });
+});
+
+// 2. MODALS
+// Contact Form Modal
 $(document).ready(function() {
     // Event listener using event delegation for all li elements under #p-menu
     $('#close').on("click", function() {
